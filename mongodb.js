d5e3f10f85819f31290fb9aeaf16de633f6b01e6
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-04 21:14:04 +0200

    insering users

--- mongodb.js
+++ mongodb.js
@@ -1,3 +1,26 @@
-const mongodb = require('mongodb');
+const Mongo = require('mongodb').MongoClient
 
-const MongoClient = mongodb.MongoClient;
+// MongoDB database connection
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+Mongo.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+    const db = client.db(databaseName);
+
+    db.collection('users')
+        .insertOne({ name: 'Andrew', age: 27 })
+        .then(res => { console.log(res) }, err => { console.log(err) });
+
+    db.collection('users')
+        .insertMany([
+            { name: 'Filip', age: 17 },
+            { name: 'Mateusz', age: 26 },
+            { name: 'Aleksander', age: 25 }
+        ]).then(
+            res => {console.log(res)},
+            err => {console.log(err)}
+        );
+});
