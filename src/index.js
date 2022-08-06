4fbdd101a7e05549d24d6c1b36b39f401b589967
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-06 15:04:01 +0200

    creating post endpoint for adding task

--- src/index.js
+++ src/index.js
@@ -2,21 +2,27 @@ const express = require('express');
 require('./db/mongoose');
 
 const User = require('./db/models/user');
-// const Task = require('./db/models/task.js');
+const Task = require('./db/models/task');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.post('/user', (req, res) => {
-    const user = new User(req.body)
+    const user = new User(req.body);
 
     user.save()
-        .then(() => (res.send(user)))
-        .catch(err => (
-            res.status(400).send(err)
-        ));
-})
+        .then(() => res.send(user))
+        .catch(err => res.status(400).send(err));
+});
+
+app.post('/task', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save()
+        .then(() => res.send(task))
+        .catch(err => res.status(400).send(err));
+});
 
 app.listen(port, () => {console.log('server is up on port' + port)});
