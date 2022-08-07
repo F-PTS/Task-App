df5b5cae14f456e387f7a09d78662f887dfd8ea0
Filip Pitus <80021800+F-PTS@users.noreply.github.com>
2022-08-07 03:16:17 +0200

    Adding RESTful api http endpoints

--- src/index.js
+++ src/index.js
@@ -9,19 +9,56 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/user', (req, res) => {
+
+// READ
+
+app.get('/users', (req, res) => {
+    User.find({})
+        .then(users => res.send(users))
+        .catch(err => res.status(500).send(err));
+});
+
+app.get('/users/:id', (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+            if(!user) return res.status(404).send();
+
+            res.send(user);
+        })
+        .catch(err => res.status(401).send(err));
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find({})
+        .then(tasks => res.send(tasks))
+        .catch(err => res.status(500).send(err));
+});
+
+app.get('/tasks/:id', (req, res) => {
+    Task.findById(req.params.id)
+        .then(task => {
+            if(!task) return res.status(404).send()
+
+            res.send(task);
+        })
+        .catch(err => res.status(401).send(err));
+})
+
+// CREATE
+
+app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save()
-        .then(() => res.send(user))
+        .then(() => res.status(201).send(user))
         .catch(err => res.status(400).send(err));
 });
 
-app.post('/task', (req, res) => {
+app.post('/tasks', (req, res) => {
     const task = new Task(req.body);
 
     task.save()
-        .then(() => res.send(task))
+        .then(() => res.status(201).send(task))
         .catch(err => res.status(400).send(err));
 });
 
